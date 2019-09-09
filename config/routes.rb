e4fed06f6a8461fdc573e03ab192b847f95e4395
nastex21 <nasax2000@gmail.com>
2019-09-09 10:36:09 -0500

    Commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   get 'welcome/index'
 
-  resources :articles do
-    resources :comment
-  end
-
   root 'welcome#index'
+
+  resources :articles do
+    resources :comments
+  end
 end
